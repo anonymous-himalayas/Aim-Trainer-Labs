9063b12d6713d15d7c938634560a5818db6af53c
Himal <hpmalik@uci.edu>
2024-03-29 17:04:56 -0700

    Rebased all files

---
+++ Aim Trainer/Targets.py
@@ -0,0 +1,33 @@
+import pygame
+import math
+
+
+class Target:
+    MAX_SIZE = 30
+    GROWTH = 0.2
+    COLOR = 'red'
+    INNER_COLOR = 'white'
+
+    def __init__(self, x, y) -> None:
+        self.x_coord = x
+        self.y_coord = y
+        self.size = 0
+        self.grow = True
+
+    def update(self):
+        if self.size + self.GROWTH >= self.MAX_SIZE:
+            self.grow = False
+        
+        if self.grow:
+            self.size += self.GROWTH
+        else:
+            self.size -= self.GROWTH
+    
+    def draw_target(self, win):
+        pygame.draw.circle(win, self.COLOR, (self.x_coord, self.y_coord), self.size)
+        pygame.draw.circle(win, self.INNER_COLOR, (self.x_coord, self.y_coord), self.size * 0.8)
+        pygame.draw.circle(win, self.COLOR, (self.x_coord, self.y_coord), self.size * 0.6)
+        pygame.draw.circle(win, self.INNER_COLOR, (self.x_coord, self.y_coord), self.size * 0.4)
+                           
+    def collide(self, x, y):
+        pass
---
+++ Aim Trainer/training_main.py
@@ -0,0 +1,69 @@
+
+import pygame
+import random
+import time
+from Targets import Target
+
+
+WIDTH, HEIGHT = 800, 600
+TARGET_TIME = 600
+TARGET_PADDING = 30
+
+
+def draw_board(win, targets):
+    win.fill((0,0,0))
+    for target in targets:
+        target.draw_target(win)
+    
+    pygame.display.update()
+
+def main():
+    pygame.init()
+    window = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('Aim Trainer')
+
+    run = True
+    targets = []
+    clock = pygame.time.Clock()
+
+    target_points = 0
+    clicks = 0
+    misses = 0
+    start_time = time.time()
+
+    pygame.time.set_timer(pygame.USEREVENT, TARGET_TIME)
+
+    while run:
+        clock.tick(60)
+        click = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+            
+            if event.type == pygame.USEREVENT:
+                x = random.randint(TARGET_PADDING, WIDTH - TARGET_PADDING)
+                y = random.randint(TARGET_PADDING, HEIGHT - TARGET_PADDING)
+                target = Target(x, y)
+                targets.append(target)
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click = True
+                clicks += 1
+
+        for target in targets:
+            target.update()
+
+            if target.size <= 0:
+                targets.remove(target)
+                misses += 1
+
+        draw_board(window, targets)
+
+    pygame.quit()
+    
+
+
+if __name__ == '__main__':
+    main()
