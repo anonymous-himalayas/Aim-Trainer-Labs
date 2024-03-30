bb858b91e4fcd614744afdfb9ff62b11a7fbf7b9
Himal <hpmalik@uci.edu>
2024-03-29 19:27:03 -0700

    Added final changes/ minor bug fixes to end screen

--- Aim Trainer/training_main.py
+++ Aim Trainer/training_main.py
@@ -32,25 +32,29 @@ def middle(window):
     return WIDTH / 2 - window.get_width() / 2
 
 def end_screen(window, current_time, score, clicks):
-    window.blit((0,0,0))
-    time_label = pygame.font.SysFont('arial', 24).render(f'Time: {int(current_time // 60):02d}:{int(round(current_time % 60, 1))}.{math.floor(int(current_time * 1000 % 1000) / 100)}', 1, 'black')
+    if clicks == 0:
+        clicks = 1 
+    window.fill((0,0,0))
+    time_label = pygame.font.SysFont('arial', 24).render(f'Time: {int(current_time // 60):02d}:{int(round(current_time % 60, 1))}.{math.floor(int(current_time * 1000 % 1000) / 100)}', 1, 'red')
     speed = round(score / current_time, 1)
-    speed_label = pygame.font.SysFont('arial', 24).render(f'Speed: {speed} t/s', 1, 'black')
+    speed_label = pygame.font.SysFont('arial', 24).render(f'Speed: {speed} t/s', 1, 'red')
     acc = round(score / clicks * 100, 1)
-    acc_label = pygame.font.SysFont('arial', 24).render(f'Accuracy: {acc}%', 1, 'black')
-    hits_label = pygame.font.SysFont('arial', 24).render(f'Hits: {score}', 1, 'black')
-    window.blit(time_label, (middle(time_label), 5))
-    window.blit(speed_label, (middle(speed_label), 5))
-    window.blit(hits_label, (middle(hits_label), 5))
-    window.blit(acc_label, (middle(acc_label), 5))
+    acc_label = pygame.font.SysFont('arial', 24).render(f'Accuracy: {acc}%', 1, 'red')
+    hits_label = pygame.font.SysFont('arial', 24).render(f'Hits: {score}', 1, 'red')
+    game_over = pygame.font.SysFont('arial', 40).render(f'GAME OVER', 1, 'red')
+    window.blit(game_over, (middle(game_over), 100))
+    window.blit(time_label, (middle(time_label), 200))
+    window.blit(speed_label, (middle(speed_label), 300))
+    window.blit(hits_label, (middle(hits_label), 400))
+    window.blit(acc_label, (middle(acc_label), 500))
 
     pygame.display.update()
 
-    clock = pygame.time.Clock()
-    clock.tick(1)
-    for _ in range(10000):
-        pass
-
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN:
+                quit()
 def main():
     pygame.init()
     window = pygame.display.set_mode((WIDTH, HEIGHT))
